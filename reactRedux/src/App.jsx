7b36de77adb4ddc14a6aad60d26a39a8de4b4689
aabhushanCD <dammardhakal426@gmail.com>
2024-07-08 21:46:50 +0545

    first redux commit

---
+++ reactRedux/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./component/header/header";
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ reactRedux/src/component/header/header.jsx
@@ -0,0 +1,35 @@
+const Header = () => {
+  return (
+    <>
+      <div class="bd-intro pt-2 ps-lg-2">
+        <div class="d-md-flex flex-md-row-reverse align-items-center justify-content-between">
+          <div class="mb-3 mb-md-0 d-flex text-nowrap">
+            <a
+              class="btn btn-sm btn-bd-light rounded-2"
+              href="https://github.com/twbs/bootstrap/blob/v5.3.3/site/content/docs/5.3/customize/overview.md"
+              title="View and edit this file on GitHub"
+              target="_blank"
+              rel="noopener"
+            >
+              View on GitHub
+            </a>
+          </div>
+          <h1 class="bd-title mb-0" id="content">
+            Customize
+          </h1>
+        </div>
+        <p class="bd-lead">
+          Learn how to theme, customize, and extend Bootstrap with Sass, a
+          boatload of global options, an expansive color system, and more.
+        </p>
+        <script
+          async=""
+          src="https://cdn.carbonads.com/carbon.js?serve=CKYIKKJL&amp;placement=getbootstrapcom"
+          id="_carbonads_js"
+        ></script>
+      </div>
+    </>
+  );
+};
+
+export default Header;
